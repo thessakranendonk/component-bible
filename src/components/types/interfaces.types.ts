c11aa13a9afb36a8d39b74a8df85559351ec91d3
Thessa Kranendonk <thessakranendonk@gmail.com>
2022-11-24 19:46:41 -0500

    created a types folder and a file for interfaces

---
+++ src/components/types/interfaces.types.ts
@@ -0,0 +1,62 @@
+// HEADERS AND NAV BAR
+
+export interface NavigationLink {
+  name: string;
+  href: string;
+}
+
+export interface IconHeaderProps {
+  navigationLinks: NavigationLink[];
+  iconNavBarItems?: IconNavBarProps[];
+  headerBgColor?: string;
+  linkClassName: string;
+  hoverClassName: string;
+  activeLinkClassName: string;
+  currentActiveLocation?: string;
+  buttonBgColor?: string;
+  buttonTextColor?: string;
+  buttonBorderColor?: string;
+  headerTitle?: string;
+  logo?: string | undefined;
+  logoClassName?: string;
+  alt?: string;
+  onLinkClick?: () => void;
+}
+
+export interface IconNavBarProps {
+  icon: any;
+  iconLink: string;
+  title: string;
+  undertext: string;
+}
+
+// HERO IMAGE
+
+export interface HeroOneProps {
+  image?: string;
+  alt?: string;
+  heroImageClassName?: string;
+  location?: string;
+  locationClassName?: string;
+  companyName?: string;
+  companyNameClassName?: string;
+  infoText?: string;
+  infoTextClassName?: string;
+  contactButtonText?: string;
+  contactButtonClassName?: string;
+  buttonLink?: string;
+}
+
+// ALERTS, POPUP'S, MODAL'S
+
+export interface AlertPopupProps {
+  position: string;
+  variant: string;
+  variant_color: string;
+  icon_color: string;
+  text_color: string;
+  title: string;
+  onClick: (event: React.MouseEvent<HTMLButtonElement> | React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
+  alertVisible: boolean;
+  closeable?: boolean;
+}
