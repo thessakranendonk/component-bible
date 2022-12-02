de08190b9135355eb312751938295c80b7b1737e
Thessa Kranendonk <thessakranendonk@gmail.com>
2022-12-02 10:19:57 -0500

    Added the exports for LocationMap and MapInfoSidePanel

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,5 @@ export { default as AlertPopup } from './AlertPopup';
 export { default as HeroOne } from './HeroOne';
 export { default as HeroInfoBar } from './HeroInfoBar';
 export { default as IconHeader } from './IconHeader';
+export { default as LocationMap } from './LocationMap';
+export { default as MapInfoSidePanel } from './MapInfoSidePanel';
