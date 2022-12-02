3e174f6123d71b853f5ee732a4d65f6d74b28f55
Thessa Kranendonk <thessakranendonk@gmail.com>
2022-12-02 10:18:02 -0500

    imported leaflet icons and css in index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import 'leaflet/dist/leaflet.css';
+// Leaflet icons is necessary to get icons working with webpack.
+import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css'; // Re-uses images from ~leaflet package
+import 'leaflet-defaulticon-compatibility';
 
 ReactDOM.render(
   <React.StrictMode>
