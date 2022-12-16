19e62c74b599f245f9e7fac4eeeb4a4efc107849
Thessa Kranendonk <thessakranendonk@gmail.com>
2022-12-16 11:56:59 -0500

    Added new tab header component to index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,7 @@ export { default as AlertPopup } from './AlertPopup';
 export { default as HeroOne } from './HeroOne';
 export { default as HeroInfoBar } from './HeroInfoBar';
 export { default as IconHeader } from './IconHeader';
+export { default as TabHeader } from './TabHeader';
 // export { default as LocationMap } from './LocationMap';
 export { default as MapInfoSidePanel } from './MapInfoSidePanel';
 export { default as ArrowedCarousel } from './ArrowedCarousel';
